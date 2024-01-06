6b45f634002571043aae4da9860a799a6714d75b
nikimajidifard <nikimajidifard.mfa@gmail.com>
2024-01-06 06:26:30 +0330

    UserInterface

---
+++ Wallet.Application/Contracts/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Application.DTOs;
+
+namespace Wallet.Application.Contracts
+{
+    public interface IUser
+    {
+        string CreateUser(UserDto user, int CompanyId);
+        string DeleteUser(UserDto user, int UserId);
+        string UpdateUser(UserDto user);
+        List<UserDto> GetALLUsers();
+        UserDto GetUser(int UserId);
+    }
+}
