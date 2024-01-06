702b5187f361bb135c762e20807bee87f0686d86
nikimajidifard <nikimajidifard.mfa@gmail.com>
2024-01-06 06:41:58 +0330

    Label-interface

---
+++ Wallet.Application/Contracts/ILabel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Application.DTOs;
+
+namespace Wallet.Application.Contracts
+{
+    public interface ILabel
+    {
+        string CreateLabel(string LabelName, float DesiredAmount);
+        string UpdateLabel(string LabelName, float CurrentDesiredAmount);
+        string DeleteLabel(string LabelName);
+        LabelDto GetLabel(string LabelName);
+        List<LabelDto> GetAllLabels();
+    }
+}
