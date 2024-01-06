901be9681c67f8c621e12a1b77e9f33c8b3d3cf5
nikimajidifard <nikimajidifard.mfa@gmail.com>
2024-01-06 07:06:11 +0330

    add voucher entity

---
+++ wallet.Domain/Entities/Voucher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wallet.Domain.Enums;
+
+namespace wallet.Domain.Entities
+{
+    public class Voucher
+    {
+        public int VoucherId { get; set; }
+        public int SourceVoucherId { get; set;}
+        public int DestVoucherId { get; set; }
+        public DateTime VoucherDate { get; set; }
+        public Status VoucherStatus { get; set; }
+        public float VoucherAmount { get; set; }
+
+    }
+}
