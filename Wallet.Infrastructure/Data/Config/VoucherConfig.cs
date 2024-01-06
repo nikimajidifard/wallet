18699ee5b0fea538251fb0b93664a6101dddaafc
nikimajidifard <nikimajidifard.mfa@gmail.com>
2024-01-06 22:45:10 +0330

    identify db

---
+++ Wallet.Infrastructure/Data/Config/VoucherConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wallet.Domain.Entities;
+
+namespace Wallet.Infrastructure.Data.Config
+{
+    internal class VoucherConfig : IEntityTypeConfiguration<Voucher>
+    {
+        public void Configure(EntityTypeBuilder<Voucher> builder)
+        {
+            builder.ToTable("voucher");
+            builder.HasKey(x => x.VoucherId);
+            builder.Property(x => x.VoucherAmount).HasDefaultValue(0.0);
+        }
+    }
+}
--- Wallet.Infrastructure/Data/WalletDBContext.cs
+++ Wallet.Infrastructure/Data/WalletDBContext.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using wallet.Domain.Entities;
+using Wallet.Infrastructure.Data.Config;
 
 namespace Wallet.Infrastructure.Data
 {
@@ -19,7 +20,33 @@ namespace Wallet.Infrastructure.Data
         public DbSet<SMSNotification> SMSNotifications { get; set; }
         public DbSet<PushNotification> PushNotifications { get; set; }
         public DbSet<EmailNotification> EmailNotifications { get; set; }
-        public DbSet<Label> Labels { get; set; }
+        public DbSet<Label> Labels { get; set; } 
+        public DbSet<Voucher> Vouchers { get; set; }
+        public WalletDBContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // to identify to db our configurations
+        {
+            modelBuilder.ApplyConfiguration(new UserConfig());
+            modelBuilder.ApplyConfiguration(new WalletEConfig());
+            modelBuilder.ApplyConfiguration(new CompanyConfig());
+            modelBuilder.ApplyConfiguration(new TransactionConfig());
+            modelBuilder.ApplyConfiguration(new NotificationConfig());
+            modelBuilder.ApplyConfiguration(new SMSNotificationConfig());
+            modelBuilder.ApplyConfiguration(new PushNotificationConfig());
+            modelBuilder.ApplyConfiguration(new EmailNotificationConfig());
+            modelBuilder.ApplyConfiguration(new LabelConfig());
+            modelBuilder.ApplyConfiguration(new VoucherConfig());
+        }
+        public class BloggingContextFactory : IDesignTimeDbContextFactory<WalletDBContext>
+        {
+            public WalletDBContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<WalletDBContext>();
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-CMR9S4V;Database=DemoDb;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
+                return new WalletDBContext(optionsBuilder.Options);
+
+            }
+       
+        }
 
     }
 }
