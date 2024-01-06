9d936d7d93c33574e78e8414021ca63d8d9d67dc
nikimajidifard <nikimajidifard.mfa@gmail.com>
2024-01-06 06:32:38 +0330

    Company-interface

---
+++ Wallet.Application/Contracts/ICompany.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Application.DTOs;
+
+namespace Wallet.Application.Contracts
+{
+    public interface ICompany
+    {
+        string CreateCompany(CompanyDto Company);
+        string DeleteCompany(CompanyDto Company);
+        string UpdateCompany(CompanyDto Company);
+        List<CompanyDto> GetALLCompanies();
+        CompanyDto GetCompany(int CompanyId);
+    }
+}
